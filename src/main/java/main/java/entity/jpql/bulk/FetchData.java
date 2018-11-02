0de21063384e5e636d94da4ad4620899e2777b9d
Daniyar <dan_ntu@mail.ru>
2018-11-02 14:22:41 +0600

    JPQL Fetch

---
+++ src/main/java/main/java/entity/jpql/bulk/FetchData.java
@@ -0,0 +1,29 @@
+package main.java.entity.jpql.bulk;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
+
+public class FetchData {
+  public static void main(String[] args) {
+    EntityManagerFactory entityManagerFactory =
+            Persistence.createEntityManagerFactory("jpql_bulk");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+    entityManager.getTransaction().begin();
+
+    Query query = entityManager.createQuery("select s from StudentEntity s");
+    List<StudentEntity> list = (List<StudentEntity>) query.getResultList();
+
+    list.forEach(studentEntity -> System.out.println(
+            studentEntity.getId()
+                    + " " + studentEntity.getName()
+                    + " " + studentEntity.getAge()
+    ));
+
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    entityManagerFactory.close();
+  }
+}
---
+++ src/main/java/main/java/entity/jpql/bulk/StudentEntity.java
@@ -0,0 +1,28 @@
+package main.java.entity.jpql.bulk;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+@Setter
+@Getter
+public class StudentEntity {
+  @Id
+  private int id;
+  private String name;
+  private int age;
+
+  public StudentEntity(int id, String name, int age) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+  }
+
+  public StudentEntity() {
+  }
+}
