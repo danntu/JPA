262ceff880b841c676a432bf42f6a5368affa487
Daniyar <dan_ntu@mail.ru>
2018-11-03 14:36:12 +0600

    JPQL Update

--- src/main/java/main/java/entity/jpql/bulk/FetchData.java
+++ src/main/java/main/java/entity/jpql/bulk/FetchData.java
@@ -1,10 +1,10 @@
 package main.java.entity.jpql.bulk;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
-import java.util.List;
 
 public class FetchData {
   public static void main(String[] args) {
