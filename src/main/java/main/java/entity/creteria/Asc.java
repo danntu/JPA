0322c78b4eb89fd6f301de46b48d13be649b12c1
Daniyar <dan_ntu@mail.ru>
2018-11-07 16:06:01 +0600

    JPQL Greater Than and Less Than

--- src/main/java/main/java/entity/creteria/Asc.java
+++ src/main/java/main/java/entity/creteria/Asc.java
@@ -47,6 +47,39 @@ public class Asc {
       System.out.println(studentEntity2.getAge());
     });
 
+    criteriaQuery.where(criteriaBuilder.greaterThan(studentEntity2Root.get("age"), 22));
+    CriteriaQuery<StudentEntity2> select1 = ((CriteriaQuery<StudentEntity2>) criteriaQuery).select(studentEntity2Root);
+    TypedQuery<StudentEntity2> typedQuery1 = entityManager.createQuery(select1);
+    List<StudentEntity2> list1 = typedQuery1.getResultList();
+
+    System.out.println("Students having age greater than 22");
+
+    System.out.print("s_id");
+    System.out.print("\t s_name");
+    System.out.println("\t s_age");
+    list1.forEach(studentEntity2 -> {
+      System.out.print(studentEntity2.getId() + " ");
+      System.out.print(studentEntity2.getName() + " ");
+      System.out.println(studentEntity2.getAge());
+    });
+
+    criteriaQuery.where(criteriaBuilder.lessThan(studentEntity2Root.get("age"), 22));
+    select1 = ((CriteriaQuery<StudentEntity2>) criteriaQuery).select(studentEntity2Root);
+    typedQuery1 = entityManager.createQuery(select1);
+    list1 = typedQuery1.getResultList();
+
+    System.out.println("Students having age greater than 22");
+
+    System.out.print("s_id");
+    System.out.print("\t s_name");
+    System.out.println("\t s_age");
+    list1.forEach(studentEntity2 -> {
+      System.out.print(studentEntity2.getId() + " ");
+      System.out.print(studentEntity2.getName() + " ");
+      System.out.println(studentEntity2.getAge());
+    });
+
+
     entityManager.close();
     entityManagerFactory.close();
   }
